939d51809ac583fc5a16f5bbf4c6fc872a402bc7
martucazpo <martucazpo@gmail.com>
2019-08-02 14:45:05 -0700

    database schema and seed data added

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(80) NOT NULL,
+    devoured tinyint, NOT NULL, DEFAULT false,
+	PRIMARY KEY (id)
+);
