29170c9589fff796ade2049ea656b7391f253986
martucazpo <martucazpo@gmail.com>
2019-08-03 14:30:36 -0700

    now updates db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -20,7 +20,7 @@ router.get("/", function(req, res) {
     burger.insertOne([
       "burger_name", "devoured"
     ], [
-      req.body.burger_name, req.body.devoured
+      req.body.name, req.body.devoured
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
