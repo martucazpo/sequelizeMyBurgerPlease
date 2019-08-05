b5da15e90a5e9e9fff1552a54a9e0af3afaf4ee6
martucazpo <martucazpo@gmail.com>
2019-08-05 13:57:30 -0700

    is now working! but unstyled :(

--- config/orm.js
+++ config/orm.js
@@ -78,6 +78,7 @@ var orm = {
   },
   // An example of objColVals would be {name: panther, sleepy: true}
   updateOne: function(table, objColVals, condition, cb) {
+    console.log(objColVals);
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -31,14 +31,14 @@ router.get("/", function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
-
+    console.log(req.body);
     burger.updateOne(
       {
         devoured: req.body.devoured
       },
       condition,
       function(result) {
-        if (result.changedRows === 0) {
+        if (result.changedRows == 0) {
           // If no rows were changed, then the ID must not exist, so 404
           console.log(result);
           //return res.status(404).end();
