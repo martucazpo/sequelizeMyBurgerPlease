0e32dcf1cc0c93a2f044be2977aa68c9ad0aa94c
martucazpo <martucazpo@gmail.com>
2019-08-05 18:26:12 -0700

    added background picture

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
 
 INSERT INTO burgers (burger_name) VALUES ('Whataburger');
 INSERT INTO burgers (burger_name) VALUES ('Lucky Witchbone burger');
-INSERT INTO burgers (burger_name) VALUES ('Culver_s butter-burger');
-INSERT INTO burgers (burger_name) VALUES ('Jerry Bob_s hamburger')
